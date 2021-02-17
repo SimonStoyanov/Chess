3ddd57dd4051999e24729c81c15e0a5a65838974
SimonStoyanov <simon.stoyanov@hotmail.com>
2021-02-17 20:26:41 +0100

    added piece movement script (drag and drop)

--- Chess/Assets/Scripts/MovePiece.cs
+++ Chess/Assets/Scripts/MovePiece.cs
@@ -3,22 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MovePiece : MonoBehaviour, IPointerDownHandler
+public class MovePiece : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     bool isDragging = false;
+    GameObject selectedPiece;
+    Vector2 position;
 
     private void Update()
     {
         if (isDragging == true)
         {
+            position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+            Debug.Log(position);
+
+            selectedPiece.transform.position = position;
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         isDragging = true;
-     
+
+        selectedPiece = eventData.pointerCurrentRaycast.gameObject;
+
+
         Debug.Log("hello");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging = false;
+
+        selectedPiece = null;
     }
 }
